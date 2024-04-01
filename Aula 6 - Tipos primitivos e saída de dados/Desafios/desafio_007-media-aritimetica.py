3172d2d65a68e81effec5edc755a18379bbc09f0
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2024-04-01 14:24:56 +0000

    Create desafio_007-media-aritimetica.py

---
+++ Aula 6 - Tipos primitivos e saída de dados/Desafios/desafio_007-media-aritimetica.py
@@ -0,0 +1,19 @@
+print('='*20, end=' ')
+print('Desafio 007 - Média Aritimética', end=' ')
+print('='*20)
+
+nota1 = float(input('Digite a 1ª nota: '))
+nota2 = float(input('Digite a 2ª nota: '))
+
+media = (nota1 + nota2) / 2
+
+
+if media:
+    if media => 6:
+    print('Sua média é ', media)
+    print('Você está aprvado!')
+else:
+    print('Recuperação')
+
+
+
