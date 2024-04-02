bd1f48824f76981d9a021d5d4ddfa98fb16cc881
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2024-04-02 13:50:48 +0000

    Update desafio_012-calculo-desconto.py

---
+++ Aula 6 - Tipos primitivos e saída de dados/Desafios/desafio_012-calculo-desconto.py
@@ -0,0 +1,10 @@
+print('=' *20, end=' ')
+print('Desafio 012 - Calculo de Desconto', end=' ')
+print('=' *20)
+
+valor_produto = float(input('Informe o valor do produto: R$ '))
+
+desconto = valor_produto / 2
+
+
+print('O valor do produto com desconto é de R$ {:.2f}'.format(desconto))
