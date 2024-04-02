a34ca4b59a0c799253f98cb0634a09a8c458f52b
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2024-04-02 20:46:33 +0000

    Create desafio_014-conversor-temperatura.py

---
+++ Aula 6 - Tipos primitivos e saída de dados/Desafios/desafio_014-conversor-temperatura.py
@@ -0,0 +1,5 @@
+celsios = float(input('Informe a temperatura em °C: '))
+
+fahrenheit = (celsios * 1.8) + 32
+
+print('A temperatura de {:.1f}°C corresponde a {:.1f}°F!'.format(celsios, fahrenheit))
