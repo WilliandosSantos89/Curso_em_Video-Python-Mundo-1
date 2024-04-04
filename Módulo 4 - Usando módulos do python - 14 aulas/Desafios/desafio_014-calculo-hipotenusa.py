4b3c45a9b477a3304ccc0e1b73d078f2a823d6c7
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2024-04-04 18:30:09 +0000

    Create desafio_014-calculo-hipotenusa.py

---
+++ Módulo 4 - Usando módulos do python - 14 aulas/Desafios/desafio_014-calculo-hipotenusa.py
@@ -0,0 +1,7 @@
+print('=' *20, end=' ')
+print('Desafio 017 - Calculando a hipotenusa', end=' ')
+print('=' *20)
+
+# Solicitação de dados ao usuário
+cateto_oposto = float('Informe o cateto oposto: ')
+cateto_adjaceste = float('Informe o cateto adijascente: ')
