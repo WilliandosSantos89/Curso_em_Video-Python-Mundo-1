2498476a7d714a1752247c04c2be578ea61d1aec
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2024-04-04 18:45:31 +0000

    Update desafio_014-calculo-hipotenusa.py

--- Módulo 4 - Usando módulos do python - 14 aulas/Desafios/desafio_014-calculo-hipotenusa.py
+++ Módulo 4 - Usando módulos do python - 14 aulas/Desafios/desafio_014-calculo-hipotenusa.py
@@ -1,7 +1,13 @@
+
+
 print('=' *20, end=' ')
 print('Desafio 017 - Calculando a hipotenusa', end=' ')
 print('=' *20)
 
 # Solicitação de dados ao usuário
-cateto_oposto = float('Informe o cateto oposto: ')
-cateto_adjaceste = float('Informe o cateto adijascente: ')
+cateto_oposto = float(input('Informe o cateto oposto: '))
+cateto_adjaceste = float(input('Informe o cateto adijascente: '))
+
+hipotenusa = (cateto_oposto ** 2 + cateto_adjaceste ** 2) ** 0.5
+
+print('{:.2f}'.format(hipotenusa))
