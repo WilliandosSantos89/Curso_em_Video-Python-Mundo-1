f28fb0b34f83a94fd69009484b12cf48b5ae94ce
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2024-04-05 19:45:28 +0000

    Update desafio_019-sorteador-um-item.py

--- Módulo 4 - Usando módulos do python - 14 aulas/Desafios/desafio_019-sorteador-um-item.py
+++ Módulo 4 - Usando módulos do python - 14 aulas/Desafios/desafio_019-sorteador-um-item.py
@@ -4,6 +4,15 @@ print('=' *20, end=' ')
 print('Desafio 019 - Sorteando um item na lista', end=' ')
 print('=' *20)
 
-alunos = ['Pedro', 'Thiago', 'João', 'Matheus']
-print('O aluno escolhido para apagar o quadro foi {}'.format(random.choice(alunos)))
+# Solicita ao usuário os itens a serem sorteados
+a1 = input('Informe o primeiro aluno: ')
+a2 = input('Informe o segundo aluno: ')
+a3 = input('Informe o terceiro aluno: ')
+a4 = input('Informe o quarto aluno: ')
+
+# Variável que contem os itens informados pelo usuário
+sorteador = [a1, a2, a3, a4]
+
+# Mostra o resultado do sorteio
+print('O aluno escolhido para apagar o quadro foi {}'.format(random.choice(sorteador)))
 
