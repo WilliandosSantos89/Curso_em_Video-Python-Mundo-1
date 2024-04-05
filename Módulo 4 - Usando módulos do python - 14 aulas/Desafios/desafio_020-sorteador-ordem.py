578fc1e8e556dd1868d1c52ef48d840ca0938a1d
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2024-04-05 19:53:16 +0000

    Create desafio_020-sorteador-ordem.py

---
+++ Módulo 4 - Usando módulos do python - 14 aulas/Desafios/desafio_020-sorteador-ordem.py
@@ -0,0 +1,18 @@
+import random
+
+print('=' *20, end=' ')
+print('Desafio 020 - Sorteando um lista ordenada', end=' ')
+print('=' *20)
+
+# Solicita ao usuário os itens a serem sorteados
+a1 = input('Informe o primeiro aluno: ')
+a2 = input('Informe o segundo aluno: ')
+a3 = input('Informe o terceiro aluno: ')
+a4 = input('Informe o quarto aluno: ')
+
+# Variável que contem os itens informados pelo usuário
+sorteador = [a1, a2, a3, a4]
+
+# Mostra o resultado do sorteio
+print('O aluno escolhido para apagar o quadro foi {}'.format(random.choice(sorteador)))
+
