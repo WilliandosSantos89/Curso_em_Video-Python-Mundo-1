e5c8a2db9c28a3994bd5f6991b7b7caa868262ee
WilliandosSantos89 <willian.dos.santos89@gmail.com>
2024-04-07 20:09:32 +0000

    Update aula_9-manipulando-strings.py

--- Módulo 4 - Usando módulos do python - 14 aulas/Aulas/aula_9-manipulando-strings.py
+++ Módulo 4 - Usando módulos do python - 14 aulas/Aulas/aula_9-manipulando-strings.py
@@ -1,18 +1,22 @@
 #Manipulação de strings
 
 #Fatiamento
-frase = 'Curso em Vídeo Python'
+pi = 3.14159
+print(f'Valor de pi: {pi:.2f}')
 """
+frase = 'Curso em Vídeo Python'
+
 print(frase[9]) #índice 9
 print(frase[9:13]) #índice 9 ao 12
 print(frase[9:21:2]) #9 e 21: argumentos para índices / 2: pula de 2 em 2
 print(frase[:5]) #índice zero ao 4
 print(frase[15:]) #índice 15 ao último
 print(frase[9::3])
-"""
+print(frase[::-1]) #espelhamento
+
 
 #Análise
-"""
+
 print(frase.count('o'))#procura o elemento e achando, mostra o índice
 print(len(frase)) #tamanho da string
 print(frase.count('o', 0, 13))
@@ -34,9 +38,17 @@ print(frase.lstrip()) #remove espaços da extremidade esquerda (l)
 """
 
 #Divisão
+"""
 print(frase.split()) #gera uma lista com cada palavra da cadeia (espaço)
 #dividido = frase.split()
 #print(dividido[0][3]) #nesse caso, [0] representa 'Curso' e [3] 'Python'
 
 #Junção
 #print('-'.join(frase))
+
+
+#Múltiplas Linhas
+print(f"""
+
+""")
+"""
