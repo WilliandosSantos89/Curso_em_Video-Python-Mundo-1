415615f69e642e9588ac58161316d409ff895b30
Willian dos Santos <48397346+WilliandosSantos89@users.noreply.github.com>
2024-05-23 09:05:41 -0300

    Update desafio_024-verificando-primeiras-letras.py

--- Módulo 4 - Usando módulos do python - 14 aulas/Desafios/desafio_024-verificando-primeiras-letras.py
+++ Módulo 4 - Usando módulos do python - 14 aulas/Desafios/desafio_024-verificando-primeiras-letras.py
@@ -1,5 +1,17 @@
+import re
+
 print('=' *20, end=' ')
 print('Desafio 024 - Verificando as primeiras letras de um texto', end=' ')
 print('=' *20)
 
-cidade = input('Em que cidade você nasceu? ')
+
+cidade_nascimento = input('Em que cidade você nasceu? ')
+procurar = 'santo'
+
+
+if re.search(procurar, cidade_nascimento, re.IGNORECASE):
+    print(f'A cidade {cidade_nascimento} se inicia com {procurar}')
+
+else:
+    print(f'A cidade {cidade_nascimento} não se inicia com {procurar}')
+
